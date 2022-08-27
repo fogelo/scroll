3888ee7945ee5901dbb0135f569fb726c5d9694c
antonorlov <fogelo@yandex.ru>
2022-08-27 20:39:15 +0300

    frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import styled from "styled-components";
+
+const signals = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [showBlueBox, setShowBlueBox] = useState<number[]>([])
+
+    return (
+        <AppStyled>
+            {signals.map(s => <div>
+                <div className={"grey-box"}>
+                    <div className={"buttons"}>
+                        <button className={"show-blue-box-btn"}></button>
+                        <button className={"show-green-box-btn"}></button>
+                    </div>
+                </div>
+                <div className={"blue-box"}></div>
+                <div className={"green-box"}></div>
+            </div>)}
+
+        </AppStyled>
+    );
 }
+
+const AppStyled = styled.div`
+
+  .grey-box {
+    margin: 5px auto 0;
+    height: 383px;
+    width: 500px;
+    background-color: #bababa;
+
+    .buttons {
+      display: flex;
+      gap: 5px;
+      padding: 5px;
+      justify-content: end;
+
+      .show-blue-box-btn {
+        width: 50px;
+        height: 50px;
+        background-color: #bebeff;
+        cursor: pointer;
+      }
+
+      .show-green-box-btn {
+        width: 50px;
+        height: 50px;
+        background-color: #b2ffb2;
+        cursor: pointer;
+      }
+    }
+  }
+
+  .blue-box {
+    margin: 5px auto 0;
+    height: 225px;
+    width: 500px;
+    background-color: #bebeff;
+  }
+
+  .green-box {
+    margin: 5px auto 0;
+    height: 225px;
+    width: 500px;
+    background-color: #b2ffb2;
+  }
+
+`
 
 export default App;
