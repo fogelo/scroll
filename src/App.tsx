2a66af1a0315b5e76fdef9e8636fdb9d7e9ab234
fogelo <fogelo@yandex.ru>
2022-11-20 22:07:31 +0300

    анимация плавного появления используя react-t-g

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./App.css";
 import styled from "styled-components";
 import {useInView} from "react-intersection-observer";
+import {
+    CSSTransition,
+    TransitionGroup,
+} from "react-transition-group";
 
 function App() {
     const [signals, setSignals] = useState([
-        {id: 1, checked: false, type: 4, name: "inputS1"},
-        {id: 2, checked: false, type: 4, name: "inputS2"},
-        {id: 3, checked: false, type: 4, name: "inputS3"},
-        {id: 4, checked: false, type: 4, name: "inputS4"},
-        {id: 5, checked: false, type: 4, name: "inputS5"},
-        {id: 6, checked: false, type: 1, name: "inputS1_abs"},
-        {id: 7, checked: false, type: 1, name: "inputS2_abs"},
-        {id: 8, checked: false, type: 1, name: "inputS3_abs"},
-        {id: 9, checked: false, type: 1, name: "inputS4_abs"},
-        {id: 10, checked: false, type: 1, name: "inputS5_abs"},
-        {id: 11, checked: false, type: 2, name: "inputS1_rel"},
-        {id: 12, checked: false, type: 2, name: "inputS2_rel"},
-        {id: 13, checked: false, type: 2, name: "inputS3_rel"},
-        {id: 14, checked: false, type: 2, name: "inputS4_rel"},
-        {id: 15, checked: false, type: 2, name: "inputS5_rel"},
-        {id: 16, checked: false, type: 3, name: "inputS1_contr"},
-        {id: 17, checked: false, type: 3, name: "inputS2_contr"},
-        {id: 18, checked: false, type: 3, name: "inputS3_contr"},
-        {id: 19, checked: false, type: 3, name: "inputS4_contr"},
-        {id: 20, checked: false, type: 3, name: "inputS5_contr"},
-        {id: 21, checked: false, type: 0, name: "inputS1_pred"},
-        {id: 22, checked: false, type: 0, name: "inputS2_pred"},
-        {id: 23, checked: false, type: 0, name: "inputS3_pred"},
-        {id: 24, checked: false, type: 0, name: "inputS4_pred"},
-        {id: 25, checked: false, type: 0, name: "inputS5_pred"},
+        {id: 1, checked: false, type: 4, name: "inputS1", ref: useRef(null)},
+        {id: 2, checked: false, type: 4, name: "inputS2", ref: useRef(null)},
+        {id: 3, checked: false, type: 4, name: "inputS3", ref: useRef(null)},
+        {id: 4, checked: false, type: 4, name: "inputS4", ref: useRef(null)},
+        {id: 5, checked: false, type: 4, name: "inputS5", ref: useRef(null)},
+        {id: 6, checked: false, type: 1, name: "inputS1_abs", ref: useRef(null)},
+        {id: 7, checked: false, type: 1, name: "inputS2_abs", ref: useRef(null)},
+        {id: 8, checked: false, type: 1, name: "inputS3_abs", ref: useRef(null)},
+        {id: 9, checked: false, type: 1, name: "inputS4_abs", ref: useRef(null)},
+        {id: 10, checked: false, type: 1, name: "inputS5_abs", ref: useRef(null)},
+        {id: 11, checked: false, type: 2, name: "inputS1_rel", ref: useRef(null)},
+        {id: 12, checked: false, type: 2, name: "inputS2_rel", ref: useRef(null)},
+        {id: 13, checked: false, type: 2, name: "inputS3_rel", ref: useRef(null)},
+        {id: 14, checked: false, type: 2, name: "inputS4_rel", ref: useRef(null)},
+        {id: 15, checked: false, type: 2, name: "inputS5_rel", ref: useRef(null)},
+        {id: 16, checked: false, type: 3, name: "inputS1_contr", ref: useRef(null)},
+        {id: 17, checked: false, type: 3, name: "inputS2_contr", ref: useRef(null)},
+        {id: 18, checked: false, type: 3, name: "inputS3_contr", ref: useRef(null)},
+        {id: 19, checked: false, type: 3, name: "inputS4_contr", ref: useRef(null)},
+        {id: 20, checked: false, type: 3, name: "inputS5_contr", ref: useRef(null)},
+        {id: 21, checked: false, type: 0, name: "inputS1_pred", ref: useRef(null)},
+        {id: 22, checked: false, type: 0, name: "inputS2_pred", ref: useRef(null)},
+        {id: 23, checked: false, type: 0, name: "inputS3_pred", ref: useRef(null)},
+        {id: 24, checked: false, type: 0, name: "inputS4_pred", ref: useRef(null)},
+        {id: 25, checked: false, type: 0, name: "inputS5_pred", ref: useRef(null)},
         {id: 26, checked: false, type: 5, name: "omr"},
     ])
+    const [currentRef, setCurrentRef] = useState<any>(null)
 
     const inputSignals = signals.filter(s => s.type === 4)
     const absDeviationSignals = signals.filter(s => s.type === 1)
@@ -42,6 +47,10 @@ function App() {
         threshold: 0,
     });
 
+
+    useEffect(() => {
+        !inView && currentRef?.current?.scrollIntoView({block: "end", behavior: "smooth"})
+    }, [currentRef])
 
     return (
         <AppStyled>
@@ -55,24 +64,35 @@ function App() {
                 </div>)}
             </div>
             <div>
-                {inputSignals.map(inS => inS.checked ? <div key={inS.id}>
-                    <div className={"grey-box"}>
-                        {inS.name}
-                        <div className={"buttons"}>
-                            <button className={"show-blue-box-btn"}
-                                    onClick={() => setSignals(signals.map(s => s.type === 1 && s.name.includes(inS.name) ? {
-                                        ...s,
-                                        checked: !s.checked
-                                    } : s))}>1,2
-                            </button>
-                            <button className={"show-green-box-btn"}
-                                    onClick={() => setSignals(signals.map(s => s.type === 3 && s.name.includes(inS.name) ? {
-                                        ...s,
-                                        checked: !s.checked
-                                    } : s))}>3
-                            </button>
+                {inputSignals.map(inS => inS.checked ? <React.Fragment key={inS.id}>
+                    <CSSTransition
+                        // in={inS.checked}
+                        // nodeRef={inS.ref}
+                        timeout={500}
+                        classNames="input-signal"
+                        onEntered={() => setCurrentRef(inS.ref)}
+                        // unmountOnExit
+                    >
+                        <div className={"grey-box"} key={inS.id} ref={inS.ref}>
+                            {inS.name}
+                            <div className={"buttons"}>
+                                <button className={"show-blue-box-btn"}
+                                        onClick={() => setSignals(signals.map(s => s.type === 1 && s.name.includes(inS.name) ? {
+                                            ...s,
+                                            checked: !s.checked
+                                        } : s))}>1,2
+                                </button>
+                                <button className={"show-green-box-btn"}
+                                        onClick={() => setSignals(signals.map(s => s.type === 3 && s.name.includes(inS.name) ? {
+                                            ...s,
+                                            checked: !s.checked
+                                        } : s))}>3
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    </CSSTransition>
+
+
                     {
                         absDeviationSignals.map(absS => {
                                 return (
@@ -91,7 +111,8 @@ function App() {
                             }
                         )
                     }
-                </div> : "")}
+
+                </React.Fragment> : "")}
             </div>
         </AppStyled>
     );
@@ -140,6 +161,34 @@ const AppStyled = styled.div`
     height: 225px;
     width: 500px;
     background-color: #b2ffb2;
+  }
+
+
+  //анимация для линейных графиков
+  .input-signal-enter {
+    //overflow: hidden;
+    opacity: 0;
+    height: 0;
+  }
+
+  .input-signal-enter-active {
+    //overflow: hidden;
+    opacity: 1;
+    height: 225px;
+    transition: 500ms;
+  }
+
+  .input-signal-exit {
+    overflow: hidden;
+    opacity: 1;
+    height: 225px;
+  }
+
+  .input-signal-exit-active {
+    //overflow: hidden;
+    opacity: 0;
+    height: 0;
+    transition: 500ms;
   }
 
 `
